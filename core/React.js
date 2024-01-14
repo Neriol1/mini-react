e1baeeed125ae24610e6ab16616256fae4540f82
Neriol <neriol@163.com>
2024-01-14 14:35:25 +0800

    refactor(React.js)
    优化render和createTextNode

--- core/React.js
+++ core/React.js
@@ -1,31 +1,26 @@
+const TEXT_ELEMENT = 'TEXT_ELEMENT';
+
 const createElement = (type,props,...children) => {
   return {
     type,
     props:{
       ...props,
-      children:children.map(child => typeof child === 'object' ? child : createTextNode(child))
+      children:children.map(child => typeof child === 'string' ? createTextNode(child) : child )
     }
   }
 }
-const TEXT_ELEMENT = 'TEXT_ELEMENT';
 const createTextNode = (textValue) => {
   return {
     type:TEXT_ELEMENT,
     props:{
-      textValue,
+      nodeValue:textValue,
       children:[]
     }
   }
 }
 
 const render = (vdom,container) => {
-  if (vdom.type === TEXT_ELEMENT) {
-    const textNode = document.createTextNode(vdom.props.textValue);
-    container.appendChild(textNode);
-    return;
-  }
-
-  const dom = document.createElement(vdom.type);
+  const dom = vdom.type === TEXT_ELEMENT ? document.createTextNode('') : document.createElement(vdom.type);
   Object.keys(vdom.props).forEach(key => {
     if (key !== 'children') {
       dom[key] = vdom.props[key];
